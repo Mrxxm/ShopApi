bfa8664d871a0c54a5be072e3a625369520f595d
Mrxxm <362190221@qq.com>
2024-12-23 17:36:48 +0800

    配置跨域中间件

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -2,16 +2,19 @@ package initialize
 
 import (
 	"github.com/gin-gonic/gin"
+	"shop_api/user-web/middlewares"
 	"shop_api/user-web/router"
 )
 
 func Routers() *gin.Engine {
 	// 1.初始化router
 	r := gin.Default()
-	// 2.调用Group创建路由分组
+	// 2.使用中间件，解决跨域问题
+	r.Use(middlewares.Cors())
+	// 3.调用Group创建路由分组
 	ApiV1Group := r.Group("/u/v1")
-	// 3.初始化用户路由信息
+	// 4.初始化用户路由信息
 	router.InitUserRouter(ApiV1Group)
-	// 4.返回路由
+	// 5.返回路由
 	return r
 }
