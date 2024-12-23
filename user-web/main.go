5f6cd9512c5745110b45e76350eef5ef1e3bb315
Mrxxm <362190221@qq.com>
2024-12-23 13:19:05 +0800

    自定义表单验证器

--- user-web/main.go
+++ user-web/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin/binding"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
 	"shop_api/user-web/global"
 	"shop_api/user-web/initialize"
+	myvalidator "shop_api/user-web/validator"
 )
 
 func main() {
@@ -22,7 +26,18 @@ func main() {
 		return
 	}
 
-	// 5.启动服务
+	// 5.注册验证器
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		_ = v.RegisterValidation("mobile", myvalidator.ValidateMobile)
+		_ = v.RegisterTranslation("mobile", global.Trans, func(ut ut.Translator) error {
+			return ut.Add("mobile", "{0} 非法的手机号码!", true) // see universal-translator for details
+		}, func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("mobile", fe.Field())
+			return t
+		})
+	}
+
+	// 6.启动服务
 	if err := Router.Run(fmt.Sprintf(":%d", global.ServerConfig.Port)); err != nil {
 		global.GetSugar().Panic("启动服务器失败:", err.Error())
 	}
