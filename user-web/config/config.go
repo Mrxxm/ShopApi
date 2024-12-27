662218f7f157e4fe8b6afdf572d266f831bd8baa
Mrxxm <362190221@qq.com>
2024-12-27 14:58:27 +0800

    配置文件顺序调整

--- user-web/config/config.go
+++ user-web/config/config.go
@@ -1,15 +1,15 @@
 package config
 
-type UserSrvConfig struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
-}
-
 type ServerConfig struct {
 	UserSrvConfig UserSrvConfig `mapstructure:"user_srv"`
 	JWTConfig     JWTConfig     `mapstructure:"jwt"`
 	Name          string        `mapstructure:"name"`
 	Port          int           `mapstructure:"port"`
+}
+
+type UserSrvConfig struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
 }
 
 type JWTConfig struct {
