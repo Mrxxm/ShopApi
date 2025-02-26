f85a46c5bac5d018132001a6003ed455514b7a8c
Mrxxm <362190221@qq.com>
2025-02-26 14:26:28 +0800

    添加路由

--- goods-web/router/goods.go
+++ goods-web/router/goods.go
@@ -12,14 +12,18 @@ func InitGoodsRouter(Router *gin.RouterGroup) {
 	//
 	zap.S().Debug("初始化商品相关url")
 	{
-		GoodsRouter.GET("list", goods.GetGoodsList)
+		GoodsRouter.GET("list", goods.GetGoodsList) // 商品列表
 	}
 
 	GoodsRouter.Use(middlewares.JWTAuth(), middlewares.IsAdminAuth())
 	{
 		GoodsRouter.POST("new", goods.New)
-		GoodsRouter.GET("/:id", goods.Detail)
+		GoodsRouter.GET("/:id", goods.Detail) // 获取商品详情
 		GoodsRouter.DELETE("/:id", goods.Delete)
+		GoodsRouter.GET("/:id/stocks", goods.Stocks) // 获取商品的
+
+		GoodsRouter.PUT("/:id", goods.Update)
+		GoodsRouter.PATCH("/:id", goods.UpdateStatus)
 	}
 
 }
