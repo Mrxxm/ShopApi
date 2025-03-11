f7992e51e77a77f068fec410f12ce711debd9e75
Mrxxm <362190221@qq.com>
2025-03-11 09:43:50 +0800

    添加其他客户端丽娜姐

--- order-web/initialize/srv_conn.go
+++ order-web/initialize/srv_conn.go
@@ -19,4 +19,24 @@ func InitSrvConn() {
 	}
 
 	global.OrderSrvClient = proto.NewOrderClient(conn)
+
+	gConn, err := grpc.Dial(fmt.Sprintf("consul://%s:%d/%s?wait=14s", global.ServerConfig.ConsulConfig.Host, global.ServerConfig.ConsulConfig.Port, global.ServerConfig.GoodsSrvConfig.Name),
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+	)
+	if err != nil {
+		zap.S().Fatal("连接 [user_srv] 失败", err.Error())
+	}
+
+	global.GoodsSrvClient = proto.NewGoodsClient(gConn)
+
+	iConn, err := grpc.Dial(fmt.Sprintf("consul://%s:%d/%s?wait=14s", global.ServerConfig.ConsulConfig.Host, global.ServerConfig.ConsulConfig.Port, global.ServerConfig.InventorySrvConfig.Name),
+		grpc.WithInsecure(),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
+	)
+	if err != nil {
+		zap.S().Fatal("连接 [user_srv] 失败", err.Error())
+	}
+
+	global.InventorySrvClient = proto.NewInventoryClient(iConn)
 }
